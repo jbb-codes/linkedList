cafe6ca0de8eef4f8418652595e64a52721bfa7f
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-04-08 13:27:15 -0400

    docs: testing hashmap

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-import { createLinkedList } from "./script.js";
-
+// import { createLinkedList } from "./script.js";
+import { HashMap } from "./hashmap.js";
 // example uses class syntax - adjust as necessary
-const list = createLinkedList();
 
-list.append("dog");
-list.append("cat");
-list.append("parrot");
-list.append("hamster");
-list.append("snake");
-list.append("turtle");
+const test = new HashMap(); // or HashMap() if using a factory
 
-list.insertAt("fish", 3);
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
 
-console.log(list.toString());
+test.set("moon", "silver");
+// console.log(test.has("lions"));
+console.log(test.remove("moon"));
+console.log(test.get("a"));
+console.log(test.length());
+console.log(test.getCapacity());
+console.log(test.entries());
+
+console.log(test.keys());
+console.log(test.values());
