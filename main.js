6d827f9cb832627e140c31457242fd1ffb749708
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-04-10 12:05:18 -0400

    docs: testing linkedHashMap class functions

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // import { createLinkedList } from "./script.js";
-import { HashMap } from "./hashmap.js";
+// import { HashMap } from "./hashmap.js";
+import { LinkedHashMap } from "./linkedHashMap.js";
 // example uses class syntax - adjust as necessary
 
-const test = new HashMap(); // or HashMap() if using a factory
+const test = new LinkedHashMap(16); // or HashMap() if using a factory
 
 test.set("apple", "red");
 test.set("banana", "yellow");
@@ -16,14 +17,22 @@ test.set("ice cream", "white");
 test.set("jacket", "blue");
 test.set("kite", "pink");
 test.set("lion", "golden");
-
+test.set(-134, "negative");
+test.set("mouslkhkhkhkjhkhjhjkhe", "grey");
+test.set("bat", "black");
 test.set("moon", "silver");
-// console.log(test.has("lions"));
-console.log(test.remove("moon"));
-console.log(test.get("a"));
-console.log(test.length());
-console.log(test.getCapacity());
-console.log(test.entries());
 
-console.log(test.keys());
-console.log(test.values());
+// console.log(test.get("cat"));
+
+// console.log(test.has("lions"));
+
+// console.log(test.remove("carrot"));
+
+// console.log(test.getCapacity());
+// console.log(test.length());
+
+// console.log(test.getCapacity());
+
+// console.log(test.keys());
+// console.log(test.values());
+console.log(test.entries());
